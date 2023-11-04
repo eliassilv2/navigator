d751cf8335c78dc5643724fbd72245fb48dc3354
Elias <eliaskiso4@gmail.com>
2023-11-03 21:27:33 -0300

    atividade asyncstorage

--- src/routes/DrawerRoutes.js
+++ src/routes/DrawerRoutes.js
@@ -4,6 +4,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer'
 import Home from '../screens/Home'
 import Carros from '../screens/carros/Carros'
 import StackPessoa from '../screens/pessoas/StackPessoa'
+import StackPessoaAsync from '../screens/asyncstorage/StackPessoaAsync'
 
 const Drawer = createDrawerNavigator()
 
@@ -13,6 +14,7 @@ export default function DrawerRoutes() {
             <Drawer.Screen name="Home" component={Home} />
             <Drawer.Screen name="Carros" component={Carros} />
             <Drawer.Screen name="Pessoas" component={StackPessoa} />
+            <Drawer.Screen name="PessoasAsyncStorage" component={StackPessoaAsync} />
         </Drawer.Navigator>
     )
 }
